ffef8314435694869852221b28ca51a5fc450dac
sergiopassos <sergio.passos02@gmail.com>
2019-05-25 21:46:09 -0300

    Correção da importação

--- eventex/settings.py
+++ eventex/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 from decouple import config
-from db_database_url import parse as dburl
+from db_djtabase_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
